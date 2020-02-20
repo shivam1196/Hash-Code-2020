9ea4a43e24baff13d38bc01d5a2abf0abb8060d6
Ashish Malhotra <malhotraashish2108@gmail.com>
2020-02-21 02:10:36 +0530

    Update MaxCalculation.java

--- src/com/cheetah/hashcode/MaxCalculation.java
+++ src/com/cheetah/hashcode/MaxCalculation.java
@@ -1,17 +1,17 @@
 package hashcode2020;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class MaxCalculation {
 	List<Library> mainList = new ArrayList<Library>();
 	
-	public List<Library> maxCalculatedScore(List<Library> calculatedList){
+	public void maxCalculatedScore(List<Library> calculatedList){
 		if(calculatedList.size() == 0) {
-			return mainList;
+			return ;
 		}
 		Library max = new Library();
+		max.setCalculatedScore(Long.MIN_VALUE);
 		for(int i = 0; i<calculatedList.size(); i++) {
 			if(calculatedList.get(i).getCalculatedScore() > max.getCalculatedScore()) {
 				max = calculatedList.get(i);
@@ -20,6 +20,7 @@ public class MaxCalculation {
 		int indexOfMax = calculatedList.indexOf(max);
 		mainList.add(max);
 		calculatedList.remove(indexOfMax);
-		return processLibraryList(calculatedList);
+		maxCalculatedScore(calculatedList);
+		return ;
 	}
 }

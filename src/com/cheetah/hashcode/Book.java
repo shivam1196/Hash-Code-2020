9c26fb01e4e19ef8580643cf45b5fcee88b754c2
Shivam Srivastav <cheetah_shivam@Apples-MacBook-Air.local>
2020-02-28 23:42:37 +0530

    Updated Book class

---
+++ src/com/cheetah/hashcode/Book.java
@@ -0,0 +1,22 @@
+package com.cheetah.hashcode;
+
+public class Book {
+  int id;
+  int score;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+}
